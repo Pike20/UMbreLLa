edff47adba0ca5b2c26bf6be0056579830fc749a
Pike20 <89274890+Pike20@users.noreply.github.com>
2025-02-08 21:32:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-pip install flashinfer -i https://flashinfer.ai/whl/cu124/torch2.4/
+pip install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.4/
 pip install -e .
